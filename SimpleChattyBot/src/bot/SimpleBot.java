2c3abb4ff4caf30e7085e2acd392660d6cd6c587
SushanTiwari <tiwarisusan@gmail.com>
2021-07-13 17:47:25 -0500

    SimpleChattyBot Stage 1 Implementation

---
+++ SimpleChattyBot/src/bot/SimpleBot.java
@@ -0,0 +1,7 @@
+package bot;
+
+public class SimpleBot {
+    // write your code here
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2020.");
+}
